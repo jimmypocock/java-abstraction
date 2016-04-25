3eca0df2e2a981d20d99c3c6d53d0c0ce8e0f214
Jimmy Pocock <jimmypocock@yahoo.com>
2016-04-24 23:49:05 -0500

    Created CanFly interface

---
+++ src/com/completeJavaDeveloperCourse/CanFly.java
@@ -0,0 +1,8 @@
+package com.completeJavaDeveloperCourse;
+
+/**
+ * Created by jimmypocock on 4/24/16.
+ */
+public interface CanFly {
+    void fly();
+}
--- src/com/completeJavaDeveloperCourse/Penguin.java
+++ src/com/completeJavaDeveloperCourse/Penguin.java
@@ -10,6 +10,7 @@ public class Penguin extends Bird {
 
     @Override
     public void fly() {
+        super.fly();
         System.out.println("I can't fly very well.");
     }
 }
